eb581355b913f9102fcca1a2891e2f405ed98ab3
htmleros <htmlerosavjk@gmail.com>
2025-11-07 10:42:01 -0300

    Delete Actividad 10 directory

--- Actividad 10/PP-Katy/Actividad 10/Datos.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.image('images/banner.png')
-
-st.title("Aanálisis de Datos en la Facultad de Informática")
-st.subheader(" Con Python🐍")
-st.write("¿Qué sabemos de (completar sobre los datos seleccionados)...")
