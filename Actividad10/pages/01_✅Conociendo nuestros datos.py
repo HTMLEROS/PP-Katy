2b6c3322b6a89d8b9f54cc5625b14bfc51b22319
htmleros <htmlerosavjk@gmail.com>
2025-11-12 12:24:28 -0300

    Add files via upload

--- Actividad10/pages/01_✅Conociendo nuestros datos.py
+++ Actividad10/pages/01_✅Conociendo nuestros datos.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 precios =  pd.read_csv('precios.csv')
 st.title("Parte 1")
-st.header("en esta aplicasión nos hicieron hacer preguntas y respuestas sobre  mi dataset. se me ocurrió poner lo más importante del dataset que me tocó, como algunos productos  que tenía más mensione. otra de las cosas quise mostrar fue información que puede interesar, como los nombres de cada columna que tiene mi información")
+st.header("En esta aplicasión nos hicieron hacer preguntas y respuestas sobre  mi dataset. se me ocurrió poner lo más importante del dataset que me tocó, como algunos productos  que tenía más mensione. otra de las cosas quise mostrar fue información que puede interesar, como los nombres de cada columna que tiene mi información")
 st.header("Datos que encontraron")
 filas, columnas = precios.shape
 with st.expander("¿Cuántas filas y columnas tiene el dataset?"):
--- Actividad10/pages/04_encuesta.py
+++ Actividad10/pages/04_encuesta.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 redes =  pd.read_csv('uso_digitales_FINAL.csv')
 st.title("Parte 1")
-st.header("en esta parte verás unos datos de una encuesta que se realizó a los jóvenes de la técnica 9 en el año pasado y en el año anterior, sobre las aplicasiones que usaban más")
+st.header("En esta parte verás unos datos de una encuesta que se realizó a los jóvenes de la técnica 9 en el año pasado y en el año anterior, sobre las aplicasiones que usaban más")
 st.header("Datos que encontraron")
 filas, columnas = redes.shape
 with st.expander("¿Cuántas filas y columnas tiene el dataset?"):
@@ -25,5 +26,58 @@ with st.expander("¿Cuáles son los nombres de las columnas  redes?"):
    
     st.write(redes.columns)
 st.image('images/aplicacion.png')
-st.header(" estas imagenes que vez son algunas aplicaciónes más votados en la información de la encuesta")
-        
+st.header("Estas imagenes que vez son algunas aplicaciónes más votados en la información de la encuesta")
+
+
+redes= pd.read_csv('uso_digitales_FINAL.csv')
+st.title("Gráfico")
+fig,ax = plt.subplots()
+# Crear el gráfico de barras
+conteo_tipos = redes['instagram'].value_counts()
+conteo_tipos.plot(kind='bar', ax=ax, color='skyblue')
+# Agregar etiquetas y título
+ax.set_xlabel('Género')
+ax.set_ylabel('Cantidad')
+ax.set_title('la cantidad de horas que usaron cada uno intagram ')
+
+# Mostrar el gráfico
+st.pyplot(fig)       
+
+redes= pd.read_csv('uso_digitales_FINAL.csv')
+fig,ax = plt.subplots()
+# Crear el gráfico de barras
+conteo_tipos = redes['tiktok'].value_counts()
+conteo_tipos.plot(kind='bar', ax=ax, color='skyblue')
+# Agregar etiquetas y título
+ax.set_xlabel('Género')
+ax.set_ylabel('Cantidad')
+ax.set_title('la cantidad de horas que usaron cada uno tiktok ')
+
+# Mostrar el gráfico
+st.pyplot(fig)       
+
+redes= pd.read_csv('uso_digitales_FINAL.csv')
+fig,ax = plt.subplots()
+# Crear el gráfico de barras
+conteo_tipos = redes['youtube'].value_counts()
+conteo_tipos.plot(kind='bar', ax=ax, color='skyblue')
+# Agregar etiquetas y título
+ax.set_xlabel('Género')
+ax.set_ylabel('Cantidad')
+ax.set_title('la cantidad de horas que usaron cada uno youtube ')
+
+# Mostrar el gráfico
+st.pyplot(fig)       
+
+redes= pd.read_csv('uso_digitales_FINAL.csv')
+fig,ax = plt.subplots()
+# Crear el gráfico de barras
+conteo_tipos = redes['twiter'].value_counts()
+conteo_tipos.plot(kind='bar', ax=ax, color='skyblue')
+# Agregar etiquetas y título
+ax.set_xlabel('Género')
+ax.set_ylabel('Cantidad')
+ax.set_title('la cantidad de horas que usaron cada uno twiter ')
+
+# Mostrar el gráfico
+st.pyplot(fig)       
